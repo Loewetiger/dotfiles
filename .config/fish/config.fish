8e6b9765d908d063596542c5e4ecd5013b79278e
Loewetiger <loewetiger@protonmail.com>
2023-03-29 21:33:38 +0200

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function u --description 'Update all pacman and Flatpak packages'
+    paru
+    flatpak update
+    flatpak remove --unused
+end
+
+alias yt-dlp="yt-dlp --cookies-from-browser firefox"
+alias nvim="nvim --clean"
+alias ssh="kitty +kitten ssh"
+
+starship init fish | source
+zoxide init fish | source
+direnv hook fish | source
