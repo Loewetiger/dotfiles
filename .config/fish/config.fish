5dee1d6c8a7d44e74577a8a4b2c076bea51e8d1c
Loewetiger <loewetiger@protonmail.com>
2023-03-29 21:57:49 +0200

    Set default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,8 @@ alias yt-dlp="yt-dlp --cookies-from-browser firefox"
 alias nvim="nvim --clean"
 alias ssh="kitty +kitten ssh"
 
+set -gx EDITOR nvim
+
 starship init fish | source
 zoxide init fish | source
 direnv hook fish | source
