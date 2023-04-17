17c6d0116565450f99a9e0dbc42fe2dbed3557dd
Loewetiger <loewetiger@protonmail.com>
2023-04-17 20:10:53 +0200

    Switch from neovim to helix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,27 @@ function u --description 'Update all pacman and Flatpak packages'
     flatpak remove --unused
 end
 
-alias yt-dlp="yt-dlp --cookies-from-browser firefox"
-alias nvim="nvim --clean"
+function update-mirrors --description 'Fetch new mirrors and sort them by speed'
+    argparse r/restore -- $argv
 
-set -gx EDITOR nvim
+    if set -ql _flag_restore
+        sudo mv /etc/pacman.d/mirrorlist-backup /etc/pacman.d/mirrorlist
+        return 0
+    end
+
+    export TMPFILE="$(mktemp)"; \
+    sudo true; \
+    rate-mirrors --save=$TMPFILE arch --max-delay=43200 \
+      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist
+end
+
+alias yt-dlp="yt-dlp --cookies-from-browser firefox"
+alias ls="exa"
+alias l='exa -l --all --group-directories-first --git'
+alias hx="helix"
+
+set -gx EDITOR helix
 
 starship init fish | source
 zoxide init fish | source
