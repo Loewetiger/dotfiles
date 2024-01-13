4e55dde8d600e127412ee1bd85d5af72e6dd60b3
Loewetiger <loewetiger@tuta.io>
2024-01-13 13:08:10 +0100

    Disable warning for missing glyphs

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,6 +16,9 @@ config.font = wezterm.font 'FiraCode Nerd Font'
 -- Disable tab bar if only one tab is open
 config.hide_tab_bar_if_only_one_tab = true
 
+-- Don't warn about missing glyphs, just display a placeholder
+config.warn_about_missing_glyphs = false
+
 -- and finally, return the configuration to wezterm
 return config
 
