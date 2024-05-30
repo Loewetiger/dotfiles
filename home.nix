f7bbedea549d8206ffb3329786ac46df52f72eb8
Loewetiger <loewetiger@tuta.io>
2024-05-30 22:22:38 +0200

    Enable genericLinux option

--- home.nix
+++ home.nix
@@ -5,6 +5,8 @@
   # manage.
   home.username = "leon";
   home.homeDirectory = "/home/leon";
+  
+  targets.genericLinux.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
