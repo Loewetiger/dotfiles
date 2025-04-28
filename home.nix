e6384c30bda8a8b0705577c8326be6060613e3c2
Loewetiger <loewetiger@tuta.io>
2025-04-28 21:13:47 +0200

    Add systemctl-tui

--- home.nix
+++ home.nix
@@ -36,6 +36,7 @@
     lnav
     mergiraf
     oha
+    systemctl-tui
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
