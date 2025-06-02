5cf3915dafc018655bcef6b52ca6cc9a69be3c52
Loewetiger <loewetiger@tuta.io>
2025-06-02 18:40:59 +0200

    Remove fzf-fish plugin
    I've ended up basically never using it.

--- programs/shell.nix
+++ programs/shell.nix
@@ -7,10 +7,6 @@
       lg = "lazygit";
       se = "sudo --preserve-env=PATH env";
     };
-    plugins = [{
-      name = "fzf";
-      src = pkgs.fishPlugins.fzf-fish.src;
-    }];
     functions = {
       hmu = {
         description = "Fetch the newest changes from git and run home-manager switch";
@@ -21,35 +17,6 @@
           end
           home-manager switch --flake "path:."
           popd
-        '';
-      };
-      # Based on https://github.com/kidonng/dotfiles/blob/4356fb2be6db24e9433826a86f2aa1d5c5b671ed/.config/fish/functions/__fzf_preview_file_content.fish
-      preview = {
-        description = "Preview the file with the appropiate cli tool";
-        body = ''
-          set bat_args -n --color always -r :500
-          set mime (file -b --mime-type $argv)
-
-          switch $mime[1]
-            case "inode/directory"
-              eza --tree -L 3 --icons always --color always $argv
-            case "text/*"
-              bat $bat_args $argv
-            case application/json
-              bat $bat_args -l json $argv
-            case image/{gif,jpeg,png,svg+xml,webp}
-              ${pkgs.viu}/bin/viu -s -w $COLUMNS $argv
-            case "*"
-              file -b $argv
-              echo "($mime[1])"
-          end
-        '';
-      };
-      # Overwrite the preview function of fzf.fish
-      _fzf_preview_file = {
-        description = "Print a preview for the given file based on its file type.";
-        body = ''
-          preview "$argv"
         '';
       };
     };
@@ -112,10 +79,7 @@
     enableFishIntegration = true;
   };
 
-  programs.fzf = {
-    enable = true;
-    defaultOptions = [ "--preview 'preview {}'" ];
-  };
+  programs.fzf.enable = true;
 
   programs.zellij = {
     enable = true;
