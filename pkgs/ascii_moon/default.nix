664d428c0e67e58a58af48f059ab548af090b60c
Loewetiger <loewetiger@tuta.io>
2025-12-16 16:08:22 +0100

    Update ascii_moon

--- pkgs/ascii_moon/default.nix
+++ pkgs/ascii_moon/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ascii_moon";
-  version = "0.2.0";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "rockydd";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Q3X06kzUMWuiCjM5bwUSH8idbXgJ8I5vYckjvB1mJ1o=";
+    hash = "sha256-F0N/2252oEKN4SktcNQPmKWK5Tbsgv4FmEV+wXdt2pk=";
   };
 
-  cargoHash = "sha256-rikwRsTOJIvhr0BniQUd2t+WyE7ac5F70FW3IDEgYV0=";
+  cargoHash = "sha256-p4VjqWPXvPr/i5IaaTldrOgPzRzcP2JHSoT79pSC8sM=";
 
   meta = with lib; {
     description = "A TUI (Terminal User Interface) application written in Rust that displays the moon phase in ASCII art.";
