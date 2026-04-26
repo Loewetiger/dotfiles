592163e9abcf245fbc9bb632d25311cd1b575333
Loewetiger <loewetiger@tuta.io>
2026-04-26 21:26:49 +0200

    Update gitlogue to 0.9.0

--- pkgs/gitlogue/default.nix
+++ pkgs/gitlogue/default.nix
@@ -2,20 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gitlogue";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "unhappychoice";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-T9QhPWT6w6Ezdl33mPb24UB38ApZdY/JlFMWYJZw+gA=";
+    hash = "sha256-w+5X3NhHCLDXRGQx2JxpIayekMk242uia1bJSRjDDAE=";
   };
 
   nativeBuildInputs = [
     perl
   ];
 
-  cargoHash = "sha256-7MQOf/BQ5dDR7iIOKjyKah7CJuZN4OZm+CcHso7FecI=";
+  cargoHash = "sha256-Ne0dMpQJ2W/JgCXijosqXBr8B6C1XgK4KnOjByckcms=";
 
   meta = with lib; {
     description = "A cinematic Git commit replay tool for the terminal, turning your Git history into a living, animated story.";
